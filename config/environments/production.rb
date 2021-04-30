22c8a04b5e81fe358abbaba330b310a21f157370
LW-meng <liuwei6965@outlook.com>
2021-04-30 16:14:12 -0400

    Precomplie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :yui
-  # config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglifier
   
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
